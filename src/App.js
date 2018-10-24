a8c6eddcb1be542be6750e5782b865ff38bc5fb4
Patrycja Madyda <madydapatrycja97@o2.pl>
2018-10-24 16:49:55 +0200

    Dodanie Reduxa

--- src/App.js
+++ src/App.js
@@ -1,28 +1,24 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import { connect } from "react-redux";
+import { contactsFetched } from "./actions";
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+      <div>
+        ble
       </div>
     );
   }
 }
 
-export default App;
+
+const mapStateToProps = (state) => {
+  return {
+    contacts: state.contacts // (1)
+  }
+};
+const mapDispatchToProps = { contactsFetched }; // (2)
+
+export default connect(mapStateToProps, mapDispatchToProps)(App); // (3
+
