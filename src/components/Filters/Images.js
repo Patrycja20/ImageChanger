8a1da6a075bb8e27968116201b54f74452575c78
Patrycja20 <madydapatrycja97@o2.pl>
2018-10-29 23:41:25 +0100

    Dodanie filtrów z CSS3 w komponencie Images.

--- src/components/Filters/Images.js
+++ src/components/Filters/Images.js
@@ -8,33 +8,33 @@ class Images extends Component {
     return <div className="Images">
       <Row>
         <Col>Original</Col>
-        <Col>Kawa</Col>
-        <Col>Biało-czarny</Col>
+        <Col>Winter</Col>
+        <Col>Coffee with milk</Col>
       </Row>
       <Row>
-        <Col><Link to="/Filters/View"><img src={this.props.imageSRC}/></Link></Col>
-        <Col><Link to="/Filters/View"><img src={this.props.imageSRC}/></Link></Col>
-        <Col><Link to="/Filters/View"><img src={this.props.imageSRC}/></Link></Col>
+        <Col><Link to="/Filters/View"><img className="Frame" src={this.props.imageSRC}/></Link></Col>
+        <Col><Link to="/Filters/View"><img className="Winter Frame" src={this.props.imageSRC}/></Link></Col>
+        <Col><Link to="/Filters/View"><img className="CoffeeWithMilk Frame" src={this.props.imageSRC}/></Link></Col>
       </Row>
       <Row>
-        <Col>Original</Col>
-        <Col>Kawa</Col>
-        <Col>Biało-czarny</Col>
+        <Col>Summer</Col>
+        <Col>Tea</Col>
+        <Col>Black & White</Col>
       </Row>
       <Row>
-        <Col><Link to="/Filters/View"><img src={this.props.imageSRC}/></Link></Col>
-        <Col><Link to="/Filters/View"><img src={this.props.imageSRC}/></Link></Col>
-        <Col><Link to="/Filters/View"><img src={this.props.imageSRC}/></Link></Col>
+        <Col><Link to="/Filters/View"><img className="Summer Frame" src={this.props.imageSRC}/></Link></Col>
+        <Col><Link to="/Filters/View"><img className="Tea Frame" src={this.props.imageSRC}/></Link></Col>
+        <Col><Link to="/Filters/View"><img className="BlackWhite Frame" src={this.props.imageSRC}/></Link></Col>
       </Row>
       <Row>
-        <Col>Original</Col>
-        <Col>Kawa</Col>
-        <Col>Biało-czarny</Col>
+        <Col>Dirty window</Col>
+        <Col>Fairyland</Col>
+        <Col>Vice versa</Col>
       </Row>
       <Row>
-        <Col><Link to="/Filters/View"><img src={this.props.imageSRC}/></Link></Col>
-        <Col><Link to="/Filters/View"><img src={this.props.imageSRC}/></Link></Col>
-        <Col><Link to="/Filters/View"><img src={this.props.imageSRC}/></Link></Col>
+        <Col><Link to="/Filters/View"><img className="DirtyWindow Frame" src={this.props.imageSRC}/></Link></Col>
+        <Col><Link to="/Filters/View"><img className="Fairyland Frame" src={this.props.imageSRC}/></Link></Col>
+        <Col><Link to="/Filters/View"><img className="ViceVersa Frame" src={this.props.imageSRC}/></Link></Col>
       </Row>
     </div>
   }
--- src/components/Filters/View/View.js
+++ src/components/Filters/View/View.js
@@ -4,10 +4,10 @@ import { Link } from 'react-router-dom';
 
 class View extends Component {
   render() {
-    return <div className="Title2">
+    return <div className="Filters">
       <Col>
         <h2>
-          <Link className="Back2" to="/Filters">&lt;</Link>
+          <Link className="Back" to="/Filters">&lt;</Link>
           Filters -> View
         </h2>
       </Col>
