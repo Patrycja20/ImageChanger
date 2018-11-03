250b0166458ff23926655afa35503f5d2efd8ffe
Patrycja20 <madydapatrycja97@o2.pl>
2018-11-03 21:06:20 +0100

    Updates

--- precache-manifest.efd8e82de69883b02c7c2e376c935e71.js
+++ precache-manifest.efd8e82de69883b02c7c2e376c935e71.js
@@ -4,23 +4,23 @@ self.__precacheManifest = [
     "url": "/ImageChanger/static/js/runtime~main.5c47b7ec.js"
   },
   {
-    "revision": "2ff56af8d5dacdccdb07",
-    "url": "/ImageChanger/static/js/main.2ff56af8.chunk.js"
+    "revision": "8e2495947c5acae280f6",
+    "url": "/ImageChanger/static/js/main.8e249594.chunk.js"
   },
   {
     "revision": "0e4086919b87a0eca9b3",
     "url": "/ImageChanger/static/js/1.0e408691.chunk.js"
   },
   {
-    "revision": "2ff56af8d5dacdccdb07",
-    "url": "/ImageChanger/static/css/main.7ed20aeb.chunk.css"
+    "revision": "8e2495947c5acae280f6",
+    "url": "/ImageChanger/static/css/main.cb8af852.chunk.css"
   },
   {
     "revision": "0e4086919b87a0eca9b3",
     "url": "/ImageChanger/static/css/1.eda97497.chunk.css"
   },
   {
-    "revision": "9d2863ade59d06b74302127656ab4e30",
+    "revision": "f4ba8d7e6454b34e27cc2b5c61e17612",
     "url": "/ImageChanger/index.html"
   }
 ];
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/ImageChanger/precache-manifest.aa4906b155ac3ac1ea52e9332149fb07.js"
+  "/ImageChanger/precache-manifest.efd8e82de69883b02c7c2e376c935e71.js"
 );
 
 workbox.clientsClaim();
